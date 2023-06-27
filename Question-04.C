77b132940d7b2ecc1074cbad48ec0b41dcc92c77
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-27 09:19:53 +0530

    Create Question-04.C

---
+++ Question-04.C
@@ -0,0 +1,23 @@
+//Write a recursive function to print first N odd natural numbers in reverse order.
+
+#include<stdio.h>
+void oddN(int);
+
+void oddN(int n)
+{
+        if(n>0)
+        {
+                printf("%d ",2*n-1);
+                oddN(n-1);
+        }
+}
+
+int main()
+{
+        int m ;
+        printf("Enter terms : ");
+        scanf("%d",&m);
+
+        oddN(m);
+        return 0 ;
+}
